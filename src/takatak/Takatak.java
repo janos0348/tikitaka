a1af15a71adbb62dbf5691ebd9013dc7345ee7fe
Katona János (SZF_2022_2024) <katona.janos1@diak.szamalk-szalezi.hu>
2023-09-20 12:51:16 +0200

    alap kiiras ciklusban

--- src/takatak/Takatak.java
+++ src/takatak/Takatak.java
@@ -7,7 +7,10 @@ package takatak;
 public class Takatak {
 
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello Word");
+            
+        }
     }
     
 }
